ea6fa1eef4b470ab269f7371b1c8a281830da3e7
Yue JIN <yjin@nustarnuclear.com>
2022-06-01 13:54:07 +0800

    fix: rewrite cypress plugin index with typescript

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This example plugins/index.ts can be used to load plugins
 //
 // You can change the location of this file or turn off loading
 // the plugins file with the 'pluginsFile' configuration option.
@@ -12,13 +12,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-const { startDevServer } = require('@cypress/vite-dev-server')
+import { startDevServer } from '@cypress/vite-dev-server'
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
+const pluginConfig: Cypress.PluginConfig = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
   on('dev-server:start', (options) => {
@@ -26,3 +22,5 @@ module.exports = (on, config) => {
   })
   return config
 }
+
+export default pluginConfig
