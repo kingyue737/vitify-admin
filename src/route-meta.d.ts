635ad60d9d8cb61a81b078a6074ea8a94d896191
Yue JIN <yjin@nustarnuclear.com>
2022-08-08 16:59:26 +0800

    chore: update route-meta comments

--- src/route-meta.d.ts
+++ src/route-meta.d.ts
@@ -6,12 +6,12 @@ import type { Role } from '@/api/users'
 declare module 'vue-router' {
   interface RouteMeta {
     icon?: string // drawer item icon
-    drawerGroup?: 'admin' | 'PUC' // groups will be separated by divider line
-    title?: string // drawer item text
+    drawerGroup?: 'admin' | 'PUC' // groups will be separated by divider line in drawer
+    title?: string // drawer item and breadcrumb text
     subtitle?: string
     roles?: Role[] // authorized user groups
     dataCy?: string // for cypress location
-    hidden?: boolean // hide this route if True
-    breadcrumb?: 'hidden' | 'disabled'
+    hidden?: boolean // hide this route in drawer if True
+    breadcrumb?: 'hidden' | 'disabled' // default is enabled
   }
 }
