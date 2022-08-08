48e666e5e48dc985ef58808782f8cd87604eb281
Yue JIN <yjin@nustarnuclear.com>
2022-08-08 20:55:37 +0800

    fix: suppress vue warning of invalid component name

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,10 +1,14 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+export default defineComponent({
+  name: 'ErrorPage',
+})
+</script>
 
 <template>
   <div>
     <div class="wrapper">
       <v-icon class="logo mb-4">$nustar</v-icon>
-      <p class="text-h3">404 查无此页</p>
+      <p class="text-h5">404 Not Found</p>
     </div>
   </div>
 </template>
@@ -17,7 +21,7 @@
   opacity: 0.6;
 }
 .logo {
-  font-size: 15em;
+  font-size: 12em;
   opacity: 0.2;
 }
 </style>
