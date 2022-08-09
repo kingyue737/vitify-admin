906d8bb2f997aff57d09122e9f323ee61d36a212
Yue JIN <yjin@nustarnuclear.com>
2022-08-09 19:48:15 +0800

    fix: add missing default api url and groups api

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,20 +1,28 @@
 import { setupWorker, rest } from 'msw'
 import type { IUserData } from '@/api/users'
 
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  `http://${window.location.hostname}:9529/api/v1`
 const url = (path: string) => {
-  return import.meta.env.VITE_API_URL + path
+  return baseURL + path
 }
+
+const groups = [
+  { id: 1, name: 'admin' },
+  { id: 2, name: 'staff' },
+]
 
 export const worker = setupWorker(
   rest.get(url('/users/:username'), (req, res, ctx) => {
-    return res(ctx.json({ id: 99, groups: [5] }))
+    return res(ctx.json({ id: 99, groups: [1] }))
   }),
   rest.get(url('/users'), (req, res, ctx) => {
     return res(
       ctx.json<IUserData[]>([
         {
           id: 1,
-          groups: [5],
+          groups: [1],
           username: 'kingyue737',
           name: 'Yue JIN',
           email: 'yuejin13@fudan.edu.cn',
@@ -23,7 +31,7 @@ export const worker = setupWorker(
         },
         {
           id: 2,
-          groups: [4],
+          groups: [2],
           username: 'fuant',
           name: 'Antony Fu',
           date_joined: '2022-07-08T21:32:00.709Z',
@@ -31,7 +39,7 @@ export const worker = setupWorker(
         },
         {
           id: 1,
-          groups: [4],
+          groups: [2],
           name: 'Ivan You',
           username: 'xiaoyouyou',
           date_joined: '2022-07-08T12:35:09.709Z',
@@ -39,7 +47,7 @@ export const worker = setupWorker(
         },
         {
           id: 1,
-          groups: [4],
+          groups: [2],
           name: 'Johnny Leider',
           username: 'johnnyleider',
           date_joined: '2022-01-21T12:35:09.709Z',
@@ -55,5 +63,11 @@ export const worker = setupWorker(
         refresh: 'admin',
       })
     )
+  }),
+  rest.get(url('/groups'), (req, res, ctx) => {
+    return res(ctx.json(groups))
+  }),
+  rest.get(url('/groups/:id'), (req, res, ctx) => {
+    return res(ctx.json(groups.find((g) => g.id === Number(req.params.id))))
   })
 )
