d242605be5f07896be620680bb029cc375054143
Yue JIN <yjin@nustarnuclear.com>
2022-08-22 15:52:19 +0800

    revert: "feat: try removing 404 page"
    This reverts commit febdc2ae5ac805592abe49de448aff04898bef6a.

---
+++ src/pages/[...all].vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default defineComponent({
+  name: 'ErrorPage',
+})
+</script>
+
+<template>
+  <div>
+    <div class="wrapper">
+      <v-icon class="logo mb-4">$nustar</v-icon>
+      <p class="text-h5">404 Not Found</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  position: relative;
+  top: calc(50vh - 250px);
+  text-align: center;
+  opacity: 0.6;
+}
+.logo {
+  font-size: 12em;
+  opacity: 0.2;
+}
+</style>
