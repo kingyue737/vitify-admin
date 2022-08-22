02e084db8a8b35121c6be75ee7f3fd22207971fb
Yue JIN <yjin@nustarnuclear.com>
2022-08-22 16:10:47 +0800

    fix: add missing route meta type

--- src/route-meta.d.ts
+++ src/route-meta.d.ts
@@ -7,11 +7,12 @@ declare module 'vue-router' {
   interface RouteMeta {
     icon?: string // drawer item icon
     drawerGroup?: 'admin' | 'PUC' // groups will be separated by divider line in drawer
+    drawerIndex?: number // determine the order of item in drawer
     title?: string // drawer item and breadcrumb text
-    subtitle?: string
+    subtitle?: string // subtitle in drawer item
     roles?: Role[] // authorized user groups
     dataCy?: string // for cypress location
-    hidden?: boolean // hide this route in drawer if True
+    hidden?: boolean // hide this route in drawer if truthy
     breadcrumb?: 'hidden' | 'disabled' // default is enabled
   }
 }
