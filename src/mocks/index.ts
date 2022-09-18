0714b254a51b3b3a7ab32683c232d3e2069ba449
Yue JIN <yuejin13@qq.com>
2022-09-18 22:50:59 +0800

    feat: add delete user example

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -13,52 +13,56 @@ const groups = [
   { id: 2, name: 'staff' },
 ]
 
+const users = [
+  {
+    id: 1,
+    groups: [1],
+    username: 'kingyue737',
+    name: 'Yue JIN',
+    email: 'yuejin13@fudan.edu.cn',
+    joinDate: '2021-11-08T07:35:09.709Z',
+  },
+  {
+    id: 2,
+    groups: [1],
+    username: 'lodgepole',
+    name: 'Ada Zhang',
+    email: '',
+    joinDate: '2021-04-08T23:45:09.709Z',
+  },
+  {
+    id: 3,
+    groups: [2],
+    username: 'fuant',
+    name: 'Antony Fu',
+    joinDate: '2022-07-08T21:32:00.709Z',
+  },
+  {
+    id: 4,
+    groups: [2],
+    name: 'Ivan You',
+    username: 'xiaoyouyou',
+    joinDate: '2022-07-08T12:35:09.709Z',
+  },
+  {
+    id: 5,
+    groups: [2],
+    name: 'Johnny Leider',
+    username: 'johnnyleider',
+    joinDate: '2022-01-21T12:35:09.709Z',
+  },
+]
+
 export const worker = setupWorker(
   rest.get(url('/users/:id'), (req, res, ctx) => {
     return res(ctx.json({ id: 99, groups: [1] }))
   }),
   rest.get(url('/users'), (req, res, ctx) => {
-    return res(
-      ctx.json<IUserData[]>([
-        {
-          id: 1,
-          groups: [1],
-          username: 'kingyue737',
-          name: 'Yue JIN',
-          email: 'yuejin13@fudan.edu.cn',
-          joinDate: '2021-11-08T07:35:09.709Z',
-        },
-        {
-          id: 2,
-          groups: [1],
-          username: 'lodgepole',
-          name: 'Ada Zhang',
-          email: '',
-          joinDate: '2021-04-08T23:45:09.709Z',
-        },
-        {
-          id: 3,
-          groups: [2],
-          username: 'fuant',
-          name: 'Antony Fu',
-          joinDate: '2022-07-08T21:32:00.709Z',
-        },
-        {
-          id: 4,
-          groups: [2],
-          name: 'Ivan You',
-          username: 'xiaoyouyou',
-          joinDate: '2022-07-08T12:35:09.709Z',
-        },
-        {
-          id: 5,
-          groups: [2],
-          name: 'Johnny Leider',
-          username: 'johnnyleider',
-          joinDate: '2022-01-21T12:35:09.709Z',
-        },
-      ])
-    )
+    return res(ctx.json<IUserData[]>(users))
+  }),
+  rest.delete(url('/users/:id'), (req, res, ctx) => {
+    users.splice(users.map((x) => x.id).indexOf(Number(req.params.id)), 1)
+    return res(ctx.status(204))
   }),
   rest.post(url('/auth/access-token'), (req, res, ctx) => {
     return res(
