6b27f1854d2ac37295c32b957dd87f8b9bf4a6d7
Yue JIN <yuejin13@qq.com>
2023-01-28 20:34:14 +0800

    chore: upgrade postcss-preset-env

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,9 +1,7 @@
-// import postcssPresetEnv from 'postcss-preset-env'
 import type { ConfigFn } from 'postcss-load-config'
 
-const postcssPresetEnv =
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  require('postcss-preset-env') as typeof import('postcss-preset-env')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const postcssPresetEnv: typeof import('postcss-preset-env')['default'] = require('postcss-preset-env')
 
 const config: ConfigFn = ({ env }) => ({
   plugins: [postcssPresetEnv({ stage: 3 })],
