5b3c91868a3e1b095498932118db43018b355674
Yue JIN <yjin@nustarnuclear.com>
2023-04-10 12:18:09 +0800

    fix: share client types between tests and app

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,6 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
