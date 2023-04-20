b2236b27044ed8e1cbe4aae24475f9dc60b506fc
Yue JIN <yjin@nustarnuclear.com>
2023-04-20 12:21:58 +0800

    refactor: replace screenfull with vueuse composable

--- src/components/layout/ButtonFullScreen.vue
+++ src/components/layout/ButtonFullScreen.vue
@@ -1,34 +1,13 @@
 <script setup lang="ts">
-import screenfull from 'screenfull'
-
+import { useFullscreen } from '@vueuse/core'
+const { isFullscreen, toggle } = useFullscreen()
 const { t } = useI18n()
-const isFullscreen = ref(false)
-if (screenfull.isEnabled) {
-  screenfull.on('change', change)
-}
-onUnmounted(() => {
-  if (screenfull.isEnabled) {
-    screenfull.off('change', change)
-  }
-})
-function change() {
-  if (screenfull.isEnabled) {
-    isFullscreen.value = screenfull.isFullscreen
-  }
-}
-function click() {
-  if (!screenfull.isEnabled) {
-    Message.error('Your browser does not support FullScreen')
-    return false
-  }
-  screenfull.toggle()
-}
 </script>
 
 <template>
   <v-tooltip bottom>
     <template #activator="{ on }">
-      <v-btn id="fullScreen" text min-width="0" v-on="on" @click="click">
+      <v-btn text min-width="0" v-on="on" @click="toggle">
         <v-icon>
           {{ isFullscreen ? 'mdi-fullscreen-exit' : 'mdi-fullscreen' }}
         </v-icon>
