1d0d5e41783df43743ab7c454b607e4b5f09ad10
Yue JIN <yjin@nustarnuclear.com>
2023-09-20 15:57:48 +0800

    chore: vue/setup-compiler-macros no longer needed

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,9 +19,6 @@ module.exports = {
     '@vue/eslint-config-typescript/recommended',
     '@vue/eslint-config-prettier',
   ],
-  env: {
-    'vue/setup-compiler-macros': true,
-  },
   overrides: [
     {
       files: ['cypress/integration/**.spec.{js,ts,jsx,tsx}'],
