3c3265977d24cd306ef0aaeae4d5864facdc79db
Yue JIN <yjin@nustarnuclear.com>
2023-09-26 12:07:52 +0800

    feat: extract vuetify component types to a dependency

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
 /// <reference types="vite-plugin-vue-layouts/client" />
+/// <reference types="@kingyue/vuetify2-component-types" />
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string
